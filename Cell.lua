d5eaab378ce39403f935813be26b1e6f072604fa
Lukas Pioch <lukas@zgow.de>
2015-10-14 16:42:08 +0200

    3d maze

--- Cell.lua
+++ Cell.lua
@@ -1,13 +1,14 @@
 cCell = {}
 cCell.__index = cCell
 
-function cCell.new(a_Y, a_X)
+function cCell.new(a_X, a_Y, a_Z)
 	local self = setmetatable({}, cCell)
 	
-	self.m_Y = a_Y	
 	self.m_X = a_X
+	self.m_Y = a_Y
+	self.m_Z = a_Z
 	self.m_Visited = false
-	self.m_Walls = {} -- top: 1; right: 2; down: 3; left: 4
+	self.m_Walls = {} -- top: 1; down: 2; right: 3; left: 4; backward: 5; foward: 6
 	self.m_Neighbors = {}
 	
 	return self
@@ -16,24 +17,36 @@ end
 
 
 function cCell:SetNeighbors()
-	if (self.m_Y > 1) then
-		self.m_Walls[1] = 1
-		self.m_Neighbors[1] = maze[self.m_Y - 1][self.m_X]
-	end
-	
 	if (self.m_X > 1) then
-		self.m_Walls[4] = 4
-		self.m_Neighbors[4] = maze[self.m_Y][self.m_X - 1]
+		self.m_Walls[4] = 4 -- left
+		self.m_Neighbors[4] = maze[self.m_X - 1][self.m_Y][self.m_Z]
 	end
 	
-	if (self.m_Y < #maze) then
-		self.m_Walls[3] = 3
-		self.m_Neighbors[3] = maze[self.m_Y + 1][self.m_X]
+	if (self.m_X < sizeX) then
+		self.m_Walls[3] = 3 -- right
+		self.m_Neighbors[3] = maze[self.m_X + 1][self.m_Y][self.m_Z]
 	end
 	
-	if (self.m_X < #maze[1]) then
-		self.m_Walls[2] = 2
-		self.m_Neighbors[2] = maze[self.m_Y][self.m_X + 1]
+	
+	if (self.m_Y > 1) then
+		self.m_Walls[2] = 2 -- down
+		self.m_Neighbors[2] = maze[self.m_X][self.m_Y - 1][self.m_Z]
+	end
+	
+	if (self.m_Y < sizeY) then
+		self.m_Walls[1] = 1 -- top
+		self.m_Neighbors[1] = maze[self.m_X][self.m_Y + 1][self.m_Z]
+	end
+	
+	
+	if (self.m_Z > 1) then
+		self.m_Walls[5] = 5 -- backward
+		self.m_Neighbors[5] = maze[self.m_X][self.m_Y][self.m_Z - 1]
+	end
+	
+	if (self.m_Z < sizeZ) then
+		self.m_Walls[6] = 6 -- forward
+		self.m_Neighbors[6] = maze[self.m_X][self.m_Y][self.m_Z + 1]
 	end
 end 
 
@@ -42,7 +55,7 @@ end
 function cCell:GetRandomNeigbor()
 	local left = {}
 	
-	for x = 1, 4 do
+	for x = 1, 6 do
 		if ((self.m_Neighbors[x] ~= nil) and (not self.m_Neighbors[x].m_Visited) and (self.m_Walls[x] ~= nil)) then
 			table.insert(left, x)
 		end
@@ -57,21 +70,21 @@ end
 
 
 function cCell:GetOpenNeighbors()
-    local left = {}
+	local left = {}
 	
-	for x = 1, 4 do
+	for x = 1, 6 do
 		if ((self.m_Neighbors[x] ~= nil) and (self.m_Neighbors[x].m_Visited) and (self.m_Walls[x] == nil)) then
 			table.insert(left, x)
 		end
 	end
-    return left
+	return left
 end
 
 
 function cCell:GetLeft()
 	local left = {}
 	
-	for x = 1, 4 do
+	for x = 1, 6 do
 		if ((self.m_Neighbors[x] ~= nil) and (self.m_Walls[x] ~= nil)) then
 			table.insert(left, x)
 		end
@@ -81,13 +94,18 @@ function cCell:GetLeft()
 end
 
 
-function cCell:Fill(a_Out, a_OY, a_OX)
-	for y = 0, 4 do
-		if (a_Out[a_OY + y] == nil) then
-			a_Out[a_OY + y] = {}
+function cCell:CreateCells(a_Out, a_OutX, a_OutY, a_OutZ)
+	for x = 0, 4 do
+		if (a_Out[a_OutX + x] == nil) then
+			a_Out[a_OutX + x] = {}
 		end
-		for x = 0, 4 do
-			a_Out[a_OY + y][a_OX + x] = "#"
+		for y = 0, 4 do
+			if (a_Out[a_OutX + x][a_OutY + y] == nil) then
+				a_Out[a_OutX + x][a_OutY + y] = {}
+			end
+			for z = 0, 4 do
+				a_Out[a_OutX + x][a_OutY + y][a_OutZ + z] = "#"
+			end
 		end
 	end
 
@@ -95,19 +113,46 @@ function cCell:Fill(a_Out, a_OY, a_OX)
 		return
 	end
 	
-	a_Out[a_OY][a_OX + 1] = self:IsOpen(1)
-    a_Out[a_OY][a_OX + 2] = self:IsOpen(1)
-    a_Out[a_OY][a_OX + 3] = self:IsOpen(1)
-	a_Out[a_OY + 1][a_OX] = self:IsOpen(4)
-    a_Out[a_OY + 2][a_OX] = self:IsOpen(4)
-    a_Out[a_OY + 3][a_OX] = self:IsOpen(4)
-    
-    for y = 1, 3 do
-        for x = 1, 3 do
-            a_Out[a_OY + y][a_OX + x] = " " 
-        end
-    end
+	for x = 1, 3 do
+		for y = 1, 3 do
+			for z = 1, 3 do
+				a_Out[a_OutX + x][a_OutY + y][a_OutZ + z] = " "
+			end
+		end
+	end
 end
+
+
+
+function cCell:CreateCrossings(a_Out, a_OutX, a_OutY, a_OutZ)
+	if (not self.m_Visited) then
+		return
+	end
+	
+	-- top: 1; down: 2; right: 3; left: 4; backward: 5; foward: 6
+	
+	-- top
+	for x = 1, 3 do
+		for z = 1, 3 do
+			a_Out[a_OutX + x][a_OutY + 4][a_OutZ + z] = self:IsOpen(1)
+		end
+	end
+	
+	-- forward
+	for x = 1, 3 do
+		for y = 1, 3 do
+			a_Out[a_OutX + x][a_OutY + y][a_OutZ + 4] = self:IsOpen(6)
+		end
+	end
+	
+	-- right
+	for z = 1, 3 do
+		for y = 1, 3 do
+			a_Out[a_OutX + 4][a_OutY + y][a_OutZ + z] = self:IsOpen(3)
+		end
+	end
+end
+
 
 
 function cCell:IsOpen(a_ToCheck)
--- Mazegenerator.lua
+++ Mazegenerator.lua
@@ -2,84 +2,66 @@
 math.randomseed(os.time())
 math.random(); math.random(); math.random()
 
+-- top: 1; down: 2; right: 3; left: 4; backward: 5; foward: 6
 function OpenWall(a_CurrentCell, a_Dir, a_NewCell)
 	a_CurrentCell.m_Walls[a_Dir] = nil
 	
 	if (a_Dir == 1) then
-		a_NewCell.m_Walls[3] = nil	
-	elseif (a_Dir == 2) then
-		a_NewCell.m_Walls[4] = nil
-	elseif (a_Dir == 3) then
-		a_NewCell.m_Walls[1] = nil  
-	elseif (a_Dir == 4) then
 		a_NewCell.m_Walls[2] = nil
+	elseif (a_Dir == 2) then
+		a_NewCell.m_Walls[1] = nil
+	elseif (a_Dir == 3) then
+		a_NewCell.m_Walls[4] = nil
+	elseif (a_Dir == 4) then
+		a_NewCell.m_Walls[3] = nil
+	elseif (a_Dir == 5) then
+		a_NewCell.m_Walls[6] = nil
+	elseif (a_Dir == 6) then
+		a_NewCell.m_Walls[5] = nil
 	end
 end
+
 
 
 function GetInvDir(a_Dir)
 	if (a_Dir == 1) then
-		return 3
-	elseif (a_Dir == 2) then
-		return 4
-	elseif (a_Dir == 3) then
-		return 1
-	elseif (a_Dir == 4) then
 		return 2
+	elseif (a_Dir == 2) then
+		return 1
+	elseif (a_Dir == 3) then
+		return 4
+	elseif (a_Dir == 4) then
+		return 3
+	elseif (a_Dir == 5) then
+		return 6
+	elseif (a_Dir == 6) then
+		return 5
 	end
 end
 
-
-function printMaze()
-	local oy = 1
-	local ox = 1
-	-- for y = 1, sizeY do
-	-- 	for x = 1, sizeX do
-	-- 		local cell = maze[y][x]
-	-- 		if (cell.m_Char ~= nil) then
-	-- 			out[oy + 1][ox + 1] = cell.m_Char
-	-- 		end
-	-- 		ox = ox + 2
-	-- 	end
-	-- 	oy = oy + 2
-	-- 	ox = 1
-	-- end
-	
-		
-	for y = 1, #out do
-		local str = ""
-		for x = 1, #out[y] do
-			str = str .. out[y][x]
-		end
-		print(str)
-	end
-end
 
 
 function Write()
-	print(#out)
+	-- print(#out)
 	local file = io.open("laby.txt", "w")
-
-	-- Create ground
-	for y = 1, #out do
-		for x = 1, #out[y] do
-			local str = (x - 1) .. ":0:" .. (y - 1).. ":1"
-			file:write(str .. "\n")
-		end
-	end
 	
-	for y = 1, #out do
-		for x = 1, #out[y] do
-			if (out[y][x] == " ") then
-				local str = (x - 1) .. ":1:" .. (y - 1) .. ":0"
-				file:write(str .. "\n")
-			elseif (out[y][x] == "#") then
-				local str = (x - 1) .. ":1:" .. (y - 1).. ":1"
-				file:write(str .. "\n")
+	for x = 1, #out do
+		for y = 1, #out[x] do
+			for z = 1, #out[x][y] do
+				if (out[x][y][z] == " ") then
+					local str = (x - 1) .. ":" .. (y - 1) .. ":" .. (z - 1) .. ":0"
+					file:write(str .. "\n")
+				elseif (out[x][y][z] == "#") then
+					local str = (x - 1) .. ":" .. (y - 1) .. ":" .. (z - 1) .. ":1"
+					file:write(str .. "\n")
+				else
+					local str = (x - 1) .. ":" .. (y - 1) .. ":" .. (z - 1) .. ":" .. out[x][y][z]
+					file:write(str .. "\n")
+				end
 			end
 		end
 	end
-	
+
 	file:close()
 end
 
@@ -96,34 +78,41 @@ stack = cStack.new()
 
 -- generate --
 
-sizeX = 20
-sizeY = 20
+allSize = 7
+sizeX = allSize or 5 -- Width
+sizeY = allSize or 1 -- Hight
+sizeZ = allSize or 5 -- Width
 
 
 -- Create cells
-for y = 1, sizeY do
-	maze[y] = {}
-	for x = 1, sizeX do
-		maze[y][x] = cCell.new(y, x)
+for x = 1, sizeX do
+	maze[x] = {}
+	for y = 1, sizeY do
+		maze[x][y] = {}
+		for z = 1, sizeZ do
+			maze[x][y][z] = cCell.new(x, y, z)
+		end
 	end
 end
 
 
 
 -- Get Neighbors
-for y = 1, sizeY do
-	for x = 1, sizeX do
-		maze[y][x]:SetNeighbors()
+for x = 1, sizeX do
+	for y = 1, sizeY do
+		for z = 1, sizeZ do
+			maze[x][y][z]:SetNeighbors()
+		end
 	end
 end
 
 
-maxAmount = sizeY * sizeX
+maxAmount = sizeX * sizeY * sizeZ
 visitedCells = 1
-local currentCell = maze[1][1]
+local currentCell = maze[1][1][1]
 currentCell.m_Visited = true
 
-while visitedCells < maxAmount do
+while true do -- visitedCells < maxAmount do
 	local newCell, dir = currentCell:GetRandomNeigbor()
 	if (newCell ~= nil) then
 		stack:Push(currentCell)
@@ -131,79 +120,51 @@ while visitedCells < maxAmount do
 		currentCell = newCell
 		currentCell.m_Visited = true
 		visitedCells = visitedCells + 1
-	elseif (#stack.m_Stack) then
+	elseif (#stack.m_Stack ~= 0) then
 		 currentCell = stack:Pop()
-	else
+	elseif (#stack.m_Stack == 0) then
 		break
 	end
 end
 
 
--- Fill maze
-oy = 1
-ox = 1
-for y = 1, sizeY do
-	for x = 1, sizeX do
-		local cell = maze[y][x]
-		cell:Fill(out, oy, ox)
-		ox = ox + 4
+
+
+-- Create cells
+outX = 1
+outY = 1
+outZ = 1
+for x = 1, sizeX do
+	for y = 1, sizeY do
+		for z = 1, sizeZ do
+			local cell = maze[x][y][z]
+			cell:CreateCells(out, outX, outY, outZ)
+			outZ = outZ + 4
+		end
+		outY = outY + 4
+		outZ = 1
 	end
-	oy = oy + 4
-	
-	ox = 1
+	outX = outX + 4
+	outY = 1
 end
 
--- printMaze()
+
+-- Make crossings
+outX = 1
+outY = 1
+outZ = 1
+for x = 1, sizeX do
+	for y = 1, sizeY do
+		for z = 1, sizeZ do
+			local cell = maze[x][y][z]
+			cell:CreateCrossings(out, outX, outY, outZ)
+			outZ = outZ + 4
+		end
+		outY = outY + 4
+		outZ = 1
+	end
+	outX = outX + 4
+	outY = 1
+end
+
 Write()
-
--- local currentCell = maze[1][1]
--- currentCell.m_Char = "."
--- add = false
--- stack = cStack.new()
--- cells = {}
--- 
--- while true do
--- 	local leftNeighs = currentCell:GetOpenNeighbors()
--- 	if (#leftNeighs == 1) then
--- 		currentCell.m_Char = "."
--- 		
--- 		newCell = currentCell.m_Neighbors[leftNeighs[1]]
--- 		newCell.m_Neighbors[GetInvDir(leftNeighs[1])] = nil
--- 		currentCell.m_Neighbors[leftNeighs[1]] = nil
--- 		
--- 		if add then
--- 			cells[stack.m_Stack[#stack.m_Stack]]:Push(currentCell)
--- 		end
--- 
--- 		currentCell = newCell
--- 	elseif (#leftNeighs > 1) then
--- 		stack:Push(currentCell)
--- 		cells[currentCell] = cStack.new()
--- 		add = true
--- 		
--- 		currentCell.m_Char = "."
--- 		
--- 		newCell = currentCell.m_Neighbors[leftNeighs[1]]
--- 		newCell.m_Neighbors[GetInvDir(leftNeighs[1])] = nil
--- 		currentCell.m_Neighbors[leftNeighs[1]] = nil
--- 		
--- 		currentCell = newCell
--- 	else
--- 		if (#stack.m_Stack == 0) then break end
--- 		
--- 		local newCell = stack:Pop()
--- 		
--- 		local reset = cells[newCell].m_Stack
--- 		cells[newCell] = nil
--- 		currentCell.m_Char = " "
--- 		for _, cell in pairs(reset) do
--- 			cell.m_Char = " "
--- 		end
--- 		add = false
--- 		
--- 		currentCell = newCell
--- 	end
--- 	
--- 	if (currentCell.m_X == sizeX) and (currentCell.m_Y == sizeY) then break end
--- end
-
