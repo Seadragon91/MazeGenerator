f4ce75c7511c8bcf56216fac775a66ec8925fbf7
Lukas Pioch <lukas@zgow.de>
2015-10-16 12:44:38 +0200

    better command desc

--- Mazegenerator.lua
+++ Mazegenerator.lua
@@ -40,7 +40,9 @@ end
 function CommandMazeGenerator(a_Split, a_Player)
 	-- Check param
 	if ((#a_Split == 1)) then
-		a_Player:SendMessage("/maze gen [<sizeAll> <sizeX sizeY sizeZ>] - Generate a maze")
+		a_Player:SendMessage("/maze gen x - Generate a maze with x for all sizes")
+		a_Player:SendMessage("/maze gen x z - Generate a maze with x and z and one height")
+		a_Player:SendMessage("/maze gen x y z - Generate a maze with x, y and z")
 		a_Player:SendMessage("/maze pos - Set a paste location, using your location")
 		a_Player:SendMessage("/maze paste - Pastes the maze at the location")
 		return true
@@ -53,8 +55,10 @@ function CommandMazeGenerator(a_Split, a_Player)
 			-- collectgarbage()
 		end
 
-		if ((#a_Split ~= 3) and (#a_Split ~= 5)) then
-			a_Player:SendMessage("/maze gen [<sizeAll> <sizeX sizeY sizeZ>]")
+		if ((#a_Split ~= 3) and (#a_Split ~= 4) and (#a_Split ~= 5)) then
+			a_Player:SendMessage("/maze gen x - Generate a maze with x for all sizes")
+			a_Player:SendMessage("/maze gen x z - Generate a maze with x and z and one height")
+			a_Player:SendMessage("/maze gen x y z - Generate a maze with x, y and z")
 			return true
 		end
 
@@ -63,12 +67,22 @@ function CommandMazeGenerator(a_Split, a_Player)
 		if (#a_Split == 3) then
 			local sizeAll = tonumber(a_Split[3])
 			if (sizeAll == nil) then
-				a_Player:SendMessage("sizeAll is not a number")
+				a_Player:SendMessage("Input is not a number.")
 				return true
 			end
 			sizeX = sizeAll
 			sizeY = sizeAll
 			sizeZ = sizeAll
+		end
+
+		if (#a_Split == 4) then
+			sizeX = tonumber(a_Split[3])
+			sizeY = 1
+			sizeZ = tonumber(a_Split[4])
+			if ((sizeX == nil) or (sizeZ == nil)) then
+				a_Player:SendMessage("Only numbers are allowed.")
+				return true
+			end
 		end
 
 		if (#a_Split == 5) then
